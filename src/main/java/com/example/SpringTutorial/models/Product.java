a7a580b7ac57b401593c60a5c47ab5de7964ee72
pdthang4 <pdthang4@cmcglobal.vn>
2022-12-21 17:17:50 +0700

    first commit

---
+++ src/main/java/com/example/SpringTutorial/models/Product.java
@@ -0,0 +1,78 @@
+package com.example.SpringTutorial.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Product {
+    // this is "primary key"
+    @Id
+    @GeneratedValue( strategy = GenerationType.AUTO)
+    private Long id;
+    private String productName;
+    private int year;
+    private Double price;
+    private String url;
+    //
+    public Product() {}
+
+    public Product(String productName, int year, Double price, String url) {
+        this.productName = productName;
+        this.year = year;
+        this.price = price;
+        this.url = url;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", productName='" + productName + '\'' +
+                ", year=" + year +
+                ", price=" + price +
+                ", url='" + url + '\'' +
+                '}';
+    }
+}
